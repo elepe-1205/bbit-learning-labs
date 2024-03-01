13927efdc40bdf31e0e902ddb8f67614ca9daf91
Daniel Apolinar <danapolinar@berkeley.edu>
2024-02-29 20:12:23 -0800

    <commit message here>

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -7,7 +7,7 @@ class mqProducer(mqProducerInterface):
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         self.routing_key = routing_key
         self.exchange_name = exchange_name
-        setupRMQConnection(self)
+        self.setupRMQConnection(self)
     
     
 
